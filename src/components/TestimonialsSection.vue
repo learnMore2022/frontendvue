30ce85761ad9d07eaa063f4fc2694cbacc9cd64d
sourabh3110 <107127540+learnMore2022@users.noreply.github.com>
2025-02-06 00:27:49 +0100

    Update TestimonialsSection.vue

--- src/components/TestimonialsSection.vue
+++ src/components/TestimonialsSection.vue
@@ -1,8 +1,8 @@
-<template >
+<template>
     <section class="text-white mt-20" id="testimonials" data-aos="zoom-in">
         <h2 class="text-4xl font-bold text-white text-left mb-4 px-4 xl:pl-16">Testimonials</h2>
         <div class="px-4 xl:px-16">
-            <Carousel v-bind="settings" :breakpoints="breakpoints" >
+            <Carousel v-bind="settings" :breakpoints="breakpoints">
                 <Slide v-for="element in testimonials" :key="element.id">
                     <div class="carousel__item p-4">
                         <div class="w-full mx-auto bg-[#111a3e] shadow-lg border border-[#1f1641] p-5 text-white font-light mb-6">
@@ -29,52 +29,54 @@
         </div>
     </section>
 </template>
+
 <script setup>
 import { ref } from 'vue';
-import { Carousel , Navigation , Slide } from 'vue3-carousel';
+import { Carousel, Navigation, Slide } from 'vue3-carousel';
 import 'vue3-carousel/dist/carousel.css';
-//Carousel settings
-const settings =ref({
-    itemsToShow:1,
-    snapAlign:"center",
-})
 
-//Breakpoints
-const breakpoints =ref({
-    700:{
-        itemsToShow:2.5,
-        snapAlign:'center',
-    },
-    1024:{
-        itemsToShow:3,
-        snapAlign:'start'
-    }
-})
+// Carousel settings
+const settings = ref({
+    itemsToShow: 1,
+    snapAlign: "center",
+});
 
-const testimonials=ref([
+// Breakpoints
+const breakpoints = ref({
+    700: {
+        itemsToShow: 2.5,
+        snapAlign: 'center',
+    },
+    1024: {
+        itemsToShow: 3,
+        snapAlign: 'start',
+    },
+});
+
+const testimonials = ref([
     {
-        id:1,
-        fullName:'sharvil kumar singh',
-        image:'src/assets/image.png',
-        comment:'Working with Sourabh was an absolute pleasure. His expertise in web development is unparalleled, and he delivered a website that exceeded our expectations. The project was completed on time and within budget, and the final product is both visually stunning and highly functional. I highly recommend Sourabh for any web development needs!'
+        id: 1,
+        fullName: 'Sharvil Kumar Singh',
+        image: 'src/assets/image.png',
+        comment: 'Working with Sourabh was an absolute pleasure. His expertise in web development is unparalleled, and he delivered a website that exceeded our expectations. The project was completed on time and within budget, and the final product is both visually stunning and highly functional. I highly recommend Sourabh for any web development needs!',
     },
     {
-        id:2,
-        fullName:'sonam onkarnath tripathi',
-        image:'src/assets/image.png',
-        comment:'Sourabh's mobile development skills are top-notch. He transformed our app concept into a fully functional and visually appealing product. The entire process was smooth, and Sourabh was always available to address our concerns and provide valuable insights. Our app has received fantastic feedback from users, and we couldn't be happier with the results. Highly recommend Sourabh for any mobile development projects'
+        id: 2,
+        fullName: 'Sonam Onkarnath Tripathi',
+        image: 'src/assets/image.png',
+        comment: 'Sourabh\'s mobile development skills are top-notch. He transformed our app concept into a fully functional and visually appealing product. The entire process was smooth, and Sourabh was always available to address our concerns and provide valuable insights. Our app has received fantastic feedback from users, and we couldn\'t be happier with the results. Highly recommend Sourabh for any mobile development projects',
     },
     {
-        id:3,
-        fullName:'sukriti singh',
-        image:'src/assets/image.png',
-        comment:'Sourabh's technical writing skills are exceptional. He took complex technical concepts and translated them into easy-to-understand documentation that our users love. His attention to detail and ability to communicate effectively made a significant impact on our project's success. I highly recommend Sourabh for any technical writing needs!'
+        id: 3,
+        fullName: 'Sukriti Singh',
+        image: 'src/assets/image.png',
+        comment: 'Sourabh\'s technical writing skills are exceptional. He took complex technical concepts and translated them into easy-to-understand documentation that our users love. His attention to detail and ability to communicate effectively made a significant impact on our project\'s success. I highly recommend Sourabh for any technical writing needs!',
     },
-   
-])
+]);
 </script>
-<style >
-    .carousel__prev,.carousel__next {
-        color: white !important;
-    }
+
+<style>
+.carousel__prev, .carousel__next {
+    color: white !important;
+}
 </style>

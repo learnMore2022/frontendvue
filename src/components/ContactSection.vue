53a798ca25d431bb3fba800f6952c6ade0af9deb
sourabh3110 <107127540+learnMore2022@users.noreply.github.com>
2025-02-06 00:20:41 +0100

    Update ContactSection.vue

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -8,8 +8,8 @@
                 <div class="col-lg-4 col-md-4 mb-lg-0 mt-5">
                     <div class="flex mb-10 items-center">
                         <div class="p-2"
-                            style="background: #111a3e; width: 50px;height: 46px; display: flex;justify-content: center; border-radius: 50%;
-                        overflow: hidden;border: 1px solid#111a3e;backdrop-filter: blur(9px);-webkit-backdrop-filter: blur(9px);">
+                            style="background: #111a3e; width: 50px; height: 46px; display: flex; justify-content: center; border-radius: 50%;
+                        overflow: hidden; border: 1px solid #111a3e; backdrop-filter: blur(9px); -webkit-backdrop-filter: blur(9px);">
                             <img src="https://img.icons8.com/metro/50/ffffff/new-post.png" alt="new-post" class="w-6">
                         </div>
                         <div class="ml-5 text-white">
@@ -19,8 +19,8 @@
                     </div>
                     <div class="flex mb-10 items-center">
                         <div class="p-2"
-                            style="background: #111a3e; width: 50px;height: 46px; display: flex;justify-content: center; border-radius: 50%;
-                        overflow: hidden;border: 1px solid#111a3e;backdrop-filter: blur(9px);-webkit-backdrop-filter: blur(9px);">
+                            style="background: #111a3e; width: 50px; height: 46px; display: flex; justify-content: center; border-radius: 50%;
+                        overflow: hidden; border: 1px solid #111a3e; backdrop-filter: blur(9px); -webkit-backdrop-filter: blur(9px);">
                             <img src="https://img.icons8.com/ios-filled/50/ffffff/phone.png" alt="phone" class="w-6">
                         </div>
                         <div class="ml-5 text-white">
@@ -30,8 +30,8 @@
                     </div>
                     <div class="flex mb-10 items-center">
                         <div class="p-2"
-                            style="background: #111a3e; width: 50px;height: 46px; display: flex;justify-content: center; border-radius: 50%;
-                        overflow: hidden;border: 1px solid#111a3e;backdrop-filter: blur(9px);-webkit-backdrop-filter: blur(9px);">
+                            style="background: #111a3e; width: 50px; height: 46px; display: flex; justify-content: center; border-radius: 50%;
+                        overflow: hidden; border: 1px solid #111a3e; backdrop-filter: blur(9px); -webkit-backdrop-filter: blur(9px);">
                             <img src="https://img.icons8.com/ios-filled/50/ffffff/linkedin.png" alt="linkedin"
                                 class="w-6">
                         </div>
@@ -43,23 +43,23 @@
                 </div>
             </div>
             <div
-            style="background: #111a3e; width: 100%;height: 100%; border-radius: 20px;
-            overflow: hidden;border: 1px solid #111a3e;backdrop-filter: blur(9px);-webkit-backdrop-filter: blur(9px);">
+            style="background: #111a3e; width: 100%; height: 100%; border-radius: 20px;
+            overflow: hidden; border: 1px solid #111a3e; backdrop-filter: blur(9px); -webkit-backdrop-filter: blur(9px);">
             <form class="flex flex-col p-2" data-aos="zoom-in-up" >
                 <div class="mb-6">
-                    <label for="email" class="text-white block mb-2 text-sm font-medium">email</label>
-                    <input type="email" id="email" class="bg-[#111827] placeholder:[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                    <label for="email" class="text-white block mb-2 text-sm font-medium">Email</label>
+                    <input type="email" id="email" class="bg-[#111827] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                     placeholder="email@gmail.com" name="email">
                 </div>
                 <div class="mb-6">
                     <label for="subject" class="text-white block mb-2 text-sm font-medium">Subject</label>
-                    <input type="subject" id="subject" class="bg-[#111827] placeholder:[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                    <input type="text" id="subject" class="bg-[#111827] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                     placeholder="subject" name="subject">
                 </div>
                 <div class="mb-6">
                     <label for="message" class="text-white block mb-2 text-sm font-medium">Message</label>
-                    <textarea id="Message" class="bg-[#111827] placeholder:[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                    placeholder="Let's talk about ... " name="Message"></textarea>
+                    <textarea id="message" class="bg-[#111827] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                    placeholder="Let's talk about ... " name="message"></textarea>
                 </div>
                 <button class="z-1 w-[100%!important] px-6 md:px-7 py-3 rounded-full sm:w-max flex justify-center text-white bg-primary border-2 border-transparent">
                     Send Message
@@ -67,11 +67,10 @@
             </form>
         </div>
         <div class="bg-gradient-to-tr opacity-25 from-[#570cac] to-primary blur-2xl h-20 w-80 z-0 absolute -top-1/2 -left-4 transform -translate-x-2/3 
-        -translate-1/2
-        "></div>
+        -translate-1/2"></div>
         </div>
     </section>
 </template>
-<script>
 
+<script>
 </script>
